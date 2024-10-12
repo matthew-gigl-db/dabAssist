d5c94b728addf7e4dd37511780cc0274fca96921
matthew-gigl-db <matthew-gigl-db>
2024-10-12 20:06:45 +0000

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dabAssist',
-    version='0.1',
-    packages=find_packages(include=['dabAssist.dabAssist']),
-    install_requires=[
-        # List your package dependencies here
-        # subprocess and tempfile are required
-    ],
+    name='dabAssist'
+    ,version='0.1'
+    ,packages=find_packages(include=['dabAssist', 'dabAssist.*'])
+    # ,install_requires=[
+    #     # List your package dependencies here
+    #     # subprocess and tempfile are required
+    # ]
 )
