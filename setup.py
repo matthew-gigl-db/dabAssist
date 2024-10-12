714b4cc09c5d3abeee551a9823e984898c3aa0df
matthew-gigl-db <matthew-gigl-db>
2024-10-12 18:51:52 +0000

    change to *

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='dabAssist',
     version='0.1',
-    packages=find_packages(include=['dabAssist', 'dabAssist.dabAssist.py']),
+    packages=find_packages(include=['dabAssist', 'dabAssist.*']),
     install_requires=[
         # List your package dependencies here
     ],
